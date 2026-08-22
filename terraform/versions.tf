fcc7e0b659168c31e7532b1500c55c1beb4c28ad
Cloud User <ec2-user@ip-172-31-10-96.ap-south-1.compute.internal>
2026-08-22 03:53:20 +0000

    terraform file

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -9,14 +9,3 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = var.aws_region
-
-  default_tags {
-    tags = {
-      Project     = var.project_name
-      Environment = var.environment
-      ManagedBy   = "Terraform"
-    }
-  }
-}
